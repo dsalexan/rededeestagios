b4b7dc2ebff1e0f7eab2fb1b517d368d6ec79389
Danilo Alexandre <danilo.salexan@gmail.com>
2017-11-12 14:15:10 -0200

    Versão nao finalizada para acomodar textos menores

--- js/general.js
+++ js/general.js
@@ -10,14 +10,24 @@ function reDraw(){
     $('body').addClass('dummyclass').delay(0).removeClass('dummy‌​class');
 }
 
+function getNegativeOffset(element){
+    value = $(element).data('negative-offset');
+    if(value == undefined) return 0
+    else return value;
+}
+
 $(document).ready(function(){
     $(window).on('scroll', function(){
         var wn = $(window).scrollTop();
+        var wn_bot = wn + $(window).height();
         
+        /*$('#test-label').text('');*/
         $('.header-link').each(function(){
             elm = $(this).attr('id').replace('link-', '');
-            elm_top = $('.' + elm).offset().top - 81;
-            elm_bot = $('.' + elm).offset().top - 81 + $('.' + elm).height();
+            elm_top = $('.' + elm).offset().top - 81 - getNegativeOffset('.' + elm);
+            elm_bot = elm_top - 1 + $('.' + elm).outerHeight();
+
+        /*$('#test-label').text($('#test-label').text() + $(this).attr('id') + " h:" + $('.' + elm).outerHeight() + " t:" + elm_top + " b:" + elm_bot + " | ");*/
             if (wn >= elm_top && wn <= elm_bot){
                 $(this).parent().addClass('nav-hovered');
             }else{
@@ -46,5 +56,13 @@ $('.contact .cancel').click(function(){
 
 $('.header-link').click(function(){
     elm = $(this).attr('id').replace('link-', '');
-    $('html,body').animate({scrollTop: $('.' + elm).offset().top - 81},250);
+    $('html,body').animate({scrollTop: $('.' + elm).offset().top - 81 - getNegativeOffset('.' + elm)},250);
+});
+
+$('.tooltip-close').click(function(){
+    $('#tooltip').addClass('tooltip-hidden');
+});
+
+$('.body-map .map-holder:after').click(function(){
+    alert('foi');
 });
